ca2fefd2d4ac498cfaa7f94e64d5cd6ef3e7c739
ransayada <ran.sayada@gmail.com>
2022-04-13 15:07:44 +0300

    added navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,49 @@
-import './navbar.scss';
+import "./navbar.scss";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import NightsStayIcon from "@mui/icons-material/NightsStay";
+import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ChatIcon from "@mui/icons-material/Chat";
+import ListIcon from "@mui/icons-material/List";
 
 const navbar = () => {
   return (
-    <div className="navbar-container">navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <NightsStayIcon className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListIcon className="icon" />
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/2023384/pexels-photo-2023384.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className="avatar" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default navbar
+export default navbar;
